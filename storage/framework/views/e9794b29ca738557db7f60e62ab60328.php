e950ac50367f2bbb5eb6703a51d2e655789f951d
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2024-02-11 18:48:27 +0700

    Add files via upload

---
+++ storage/framework/views/e9794b29ca738557db7f60e62ab60328.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\Fate\UKM\BNCC\Backend BNCC\LnT Mid Project\LnTMidProject_BE-I_Fatecha Rizqi Putra Prasetyo\LnTMidProject\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
