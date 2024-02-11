d4d20b09bfdd175707fa4bec206a315cff5ef42f
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2024-02-11 18:48:01 +0700

    Add files via upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Dashboard</title>
+
+    <!--CSS-->
+    <link rel="stylesheet" href="{{asset('style/dashboard.css')}}">
+
+    <!--FONTS-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <h1 class="navbar-brand" href="#">PT ChipiChapa</h1>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <section class="container">
+        <div class="card-container">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--BOOTSTRAP-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
