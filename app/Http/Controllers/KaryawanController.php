c50e209683ecf9c5aa76eb98e43c67d2c42de6e8
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2024-02-13 20:10:31 +0700

    Add files via upload

---
+++ app/Http/Controllers/KaryawanController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\karyawan;
+
+class KaryawanController extends Controller
+{
+    // public function karyawanView() {
+    //     return view('dashboard');
+    // }
+    public function addKaryawanView() {
+        return view('addKaryawan');
+    }
+
+    public function addKaryawan(Request $request) {
+        karyawan::create([
+            'nama' => $request -> nama,
+            'umur' => $request -> umur,
+            'alamat' => $request -> alamat,
+            'noTelp' => $request -> noTelp
+        ]);
+        return redirect('/dashboard');
+    } 
+    public function viewKaryawan() {
+        $karyawans = karyawan::all();
+        return view('dashboard') -> with('semuaKaryawan', $karyawans);
+    }
+}
