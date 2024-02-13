0d94fea7ac5ed4607ccc959117a60f858edaaab3
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2024-02-13 07:32:10 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,9 @@
     <!--CSS-->
     <link rel="stylesheet" href="{{asset('style/dashboard.css')}}">
 
+    <!--KUTE.js-->
+    <script src="https://cdn.jsdelivr.net/npm/kute.js@2.2.4/dist/kute.min.js"></script>
+
     <!--FONTS-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -31,8 +34,9 @@
         </div>
     </nav>
     <section class="container">
+        <a href="#" class="btn btn-add">Add</a>
         <div class="card-container">
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 15rem;">
                 <div class="card-body">
                     <h5 class="card-title">Nama</h5>
                     <p class="card-text">Umur</p>
@@ -42,7 +46,7 @@
                     <a href="#" class="btn btn-delete">Delete</a>
                 </div>
             </div>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 15rem;">
                 <div class="card-body">
                     <h5 class="card-title">Nama</h5>
                     <p class="card-text">Umur</p>
@@ -52,7 +56,7 @@
                     <a href="#" class="btn btn-delete">Delete</a>
                 </div>
             </div>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 15rem;">
                 <div class="card-body">
                     <h5 class="card-title">Nama</h5>
                     <p class="card-text">Umur</p>
@@ -62,7 +66,7 @@
                     <a href="#" class="btn btn-delete">Delete</a>
                 </div>
             </div>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 15rem;">
                 <div class="card-body">
                     <h5 class="card-title">Nama</h5>
                     <p class="card-text">Umur</p>
@@ -72,7 +76,147 @@
                     <a href="#" class="btn btn-delete">Delete</a>
                 </div>
             </div>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Nama</h5>
+                    <p class="card-text">Umur</p>
+                    <p class="card-text">Alamat</p>
+                    <p class="card-text">No telp.</p>
+                    <a href="#" class="btn btn-edit">Edit</a>
+                    <a href="#" class="btn btn-delete">Delete</a>
+                </div>
+            </div>
+            <div class="card" style="width: 15rem;">
                 <div class="card-body">
                     <h5 class="card-title">Nama</h5>
                     <p class="card-text">Umur</p>
@@ -84,6 +228,27 @@
             </div>
         </div>
     </section>
+    <div class="wave-section" style="width: 150rem; overflow-x: hidden;">
+        <svg id="water-waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path id="wave1" fill="#6276DC" fill-opacity="1" d="M0,224L34.3,234.7C68.6,245,137,267,206,229.3C274.3,192,343,96,411,58.7C480,21,549,43,617,90.7C685.7,139,754,213,823,240C891.4,267,960,245,1029,218.7C1097.1,192,1166,160,1234,160C1302.9,160,1371,192,1406,208L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+            <path id="wave2" fill="#6276DC" fill-opacity="1" d="M0,288L48,293.3C96,299,192,309,288,304C384,299,480,277,576,256C672,235,768,213,864,202.7C960,192,1056,192,1152,202.7C1248,213,1344,235,1392,245.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+        </svg>
+    </div>
+    <footer style="background: #6276DC;" class="d-flex flex-wrap justify-content-between align-items-center py-3">
+        <p class="col-md-4 mb-0">&copy; 2024 PT ChipiChapa</p>
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+        </a>
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#" class="nav-link px-2">Customer Service</a></li>
+        </ul>
+    </footer>
+
+
+    <!--KUTE.js-->
+    <script>
+        var tween = KUTE.fromTo('#wave1', {path: '#wave1' }, { path: '#wave2' }, {repeat: 999, duration: 10000, yoyo:true}).start();
+    </script>
 
     <!--BOOTSTRAP-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
